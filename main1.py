91e9af6f769f246852efdc8828a1ee46165a2672
megha <mlmeghana650@gmail.com>
2021-11-10 10:32:17 +0530

    print statement was updates

--- main1.py
+++ main1.py
@@ -2,6 +2,6 @@
 n=int(input("Enter number"))
 root=n**(1/2)
 if((root*root)==(n)):
-   print("Perfect square")
+   print(" Yes given number is aPerfect square")
 else:
    print("Not a Perfect Square")
